5eda04b1fb06b7b43cb2e9b48f96a5ff53698cc7
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-10-21 15:41:08 +0530

    message if it is not connected

--- foodires/admin/includes/db.php
+++ foodires/admin/includes/db.php
@@ -1,5 +1,10 @@
 <?php 
 	
     $db = new mysqli("localhost", "root", "", "mfors");
+    if($db->connect_errno) {
+		
+		echo "SORRY FOR INCONVENIENCE ! PLEASE COME BACK LATER WE ARE CURRENTLY UNDER MAINTENANCE";
+		
+	}
 
     ?>
