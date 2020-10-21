ef63e99be81c942f2bf102f92b5362277966460f
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-10-21 21:52:39 +0530

    class logo relative reference to index page

--- foodires/admin/includes/side_wrapper.php
+++ foodires/admin/includes/side_wrapper.php
@@ -1,3 +1,7 @@
 <div class="sidebar-wrapper">
-
+<div class="logo">
+                <a href="index.php" class="simple-text">
+                    Unique Restaurant
+                </a>
+            </div>
 </div>
