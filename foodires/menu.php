1dffe2c53542336efe6f29544c241871b065a381
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-10-22 15:33:53 +0530

    category lunch is on menu now

--- foodires/menu.php
+++ foodires/menu.php
@@ -14,7 +14,7 @@
     if($get_recent->num_rows) {
         while ($rows = $get_recent -> fetch_assoc()) {
 
-            if($row['food_category'] == "breakfast") {
+            if ($row['food_category'] == "breakfast") {
                 $bfast .= "<div class='parallax_item'>
 				
 							<a href='detail.php?fid=".$row['id']."'><img src='image/FoodPics/".$row['id'].".jpg' width='80px' height='80px' /> 
@@ -28,7 +28,24 @@
 							</a>
 							
 						</div>";
-            }
+            } elseif ($row['food_category'] == "lunch") {
+				
+				$lunch .=	"<div class='parallax_item'>
+				
+							<a href='detail.php?fid=".$row['id']."'><img src='image/FoodPics/".$row['id'].".jpg' width='80px' height='80px' /> 
+							<div class='detail'>
+								
+								<h4>".$row['food_name']."</h4>
+								<p class='desc'>".substr($row['food_description'], 0, 40)."...</p>
+								<p class='price'>#".$row['food_price']."</p>
+								
+							</div>
+							<p class='clear'></p>
+							</a>
+							
+						</div>";
+				
+			} 
 
         }
 
