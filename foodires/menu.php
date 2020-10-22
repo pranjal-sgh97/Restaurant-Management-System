d8d4821a3bfd2a1d795b5c2cbd561e5f4df715a3
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-10-22 15:40:05 +0530

    html declaration and javascript library included

--- foodires/menu.php
+++ foodires/menu.php
@@ -82,8 +82,24 @@
 			}
 
         }
-
-
     }
 
     ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>foodires</title>
+    <link rel="stylesheet" href="css/main.css" />
+
+<script src="js/jquery.min.js" ></script>
+
+<script src="js/myscript.js"></script>
+</head>
+<body>
+    
+</body>
+</html>
+    
