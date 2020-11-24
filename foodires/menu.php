8dfc10101d22ed96f7705bf92531b2703290beb2
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-11-24 16:47:07 +0530

    border box in css

--- foodires/menu.php
+++ foodires/menu.php
@@ -2,33 +2,37 @@
 	
 	session_start();
 	require "admin/includes/functions.php";
-    require "admin/includes/db.php";
-
-    $bfast = "";
+	require "admin/includes/db.php";
+	
+	$bfast = "";
 	$lunch = "";
 	$dinner = "";
-    $special = "";
-    
-    $get_recent = $db->query("SELECT * FROM food");
-
-    if($get_recent->num_rows) {
-        while ($row = $get_recent->fetch_assoc()) {
-
-            if ($row['food_category'] == "breakfast") {
-                $bfast .= "<div class='parallax_item'>
+	$special = "";
+	
+	$get_recent = $db->query("SELECT * FROM food");
+	
+	if($get_recent->num_rows) {
+		
+		while($row = $get_recent->fetch_assoc()) {
+			
+			if($row['food_category'] == "breakfast") {
+				
+				$bfast .= "<div class='parallax_item'>
 				
 							<a href='detail.php?fid=".$row['id']."'><img src='image/FoodPics/".$row['id'].".jpg' width='80px' height='80px' /> 
-                            <div class='detail'>
-                            <h4>".$row['food_name']."</h4>
-								<p class='desc'>".substr($row['food_description'], 0, 40)."...</p>
-                                <p class='price'>#".$row['food_price']."</p>
-                                
-                                </div>
+							<div class='detail'>
+								
+								<h4>".$row['food_name']."</h4>
+								<p class='desc'>".substr($row['food_description'], 0, 33)."...</p>
+								<p class='price'>#".$row['food_price']."</p>
+								
+							</div>
 							<p class='clear'></p>
 							</a>
 							
 						</div>";
-            } elseif ($row['food_category'] == "lunch") {
+				
+			}elseif($row['food_category'] == "lunch") {
 				
 				$lunch .=	"<div class='parallax_item'>
 				
@@ -36,7 +40,7 @@
 							<div class='detail'>
 								
 								<h4>".$row['food_name']."</h4>
-								<p class='desc'>".substr($row['food_description'], 0, 40)."...</p>
+								<p class='desc'>".substr($row['food_description'], 0, 33)."...</p>
 								<p class='price'>#".$row['food_price']."</p>
 								
 							</div>
@@ -45,7 +49,7 @@
 							
 						</div>";
 				
-			} elseif($row['food_category'] == "dinner") {
+			}elseif($row['food_category'] == "dinner") {
 				
 				$dinner .= "<div class='parallax_item'>
 				
@@ -53,7 +57,7 @@
 							<div class='detail'>
 								
 								<h4>".$row['food_name']."</h4>
-								<p class='desc'>".substr($row['food_description'], 0, 40)."...</p>
+								<p class='desc'>".substr($row['food_description'], 0, 33)."...</p>
 								<p class='price'>#".$row['food_price']."</p>
 								
 							</div>
@@ -62,7 +66,7 @@
 							
 						</div>";
 				
-			} elseif($row['food_category'] == "special") {
+			}elseif($row['food_category'] == "special") {
 				
 				$special .= "<div class='parallax_item'>
 				
@@ -70,7 +74,7 @@
 							<div class='detail'>
 								
 								<h4>".$row['food_name']."</h4>
-								<p class='desc'>".substr($row['food_description'], 0, 40)."...</p>
+								<p class='desc'>".substr($row['food_description'], 0, 33)."...</p>
 								<p class='price'>#".$row['food_price']."</p>
 								
 							</div>
@@ -80,28 +84,44 @@
 						</div>";
 				
 			}
+			
+		}
+		
+	}else{
+		
+		
+		
+	}
+	
+?>
 
-        }
-    }
+<!Doctype html>
 
-    ?>
-
-<!DOCTYPE html>
 <html lang="en">
+
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+
+<meta name="description" content="" />
+
+<meta name="keywords" content="" />
+
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>foodires</title>
-    
+	
+<title>MFORS</title>
+
+<link rel="stylesheet" href="css/main.css" />
 
 <script src="js/jquery.min.js" ></script>
 
 <script src="js/myscript.js"></script>
+	
 </head>
-<body>
-    <?php require "includes/header.php"; ?>
 
-    <div class="parallax" onclick="remove_class()">
+<body>
+	
+<?php require "includes/header.php"; ?>
+
+<div class="parallax" onclick="remove_class()">
 	
 	<div class="parallax_head">
 		
@@ -111,6 +131,7 @@
 	</div>
 	
 </div>
+
 <div class="content" onclick="remove_class()">
 	
 	<div class="inner_content on_parallax">
@@ -119,7 +140,7 @@
 		
 		<div class="parallax_content">
 			
-        <?php echo ($bfast == "") ? "<h3 style=' text-align: center; font-weight: lighter; padding: 10px 0px; background: #ffeeee; color: #333;'>Your shopping basket is empty</h3>" : $bfast; ?>
+			<?php echo ($bfast == "") ? "<h3 style=' text-align: center; font-weight: lighter; padding: 10px 0px; background: #ffeeee; color: #333;'>Your shopping basket is empty</h3>" : $bfast; ?>
 			
 			<p class="clear"></p>
 			
@@ -128,6 +149,7 @@
 	</div>
 	
 </div>
+
 <div class="content" onclick="remove_class()">
 	
 	<div class="inner_content on_parallax">
@@ -145,6 +167,7 @@
 	</div>
 	
 </div>
+
 <div class="content" onclick="remove_class()">
 	
 	<div class="inner_content on_parallax">
@@ -162,6 +185,7 @@
 	</div>
 	
 </div>
+
 <div class="content" onclick="remove_class()">
 	
 	<div class="inner_content on_parallax">
@@ -179,6 +203,7 @@
 	</div>
 	
 </div>
+
 <div class="footer_parallax" onclick="remove_class()">
 	
 	<div class="on_footer_parallax">
@@ -188,7 +213,7 @@
 	</div>
 	
 </div>
-
+	
 </body>
+
 </html>
-    
