2d2f13a950a74407cc9b6406b44a9c71b43d0afd
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-11-24 15:53:31 +0530

    fetching data from the db

--- foodires/admin/index.php
+++ foodires/admin/index.php
@@ -9,7 +9,24 @@
     if($_SERVER['REQUEST_METHOD'] == 'POST') {
         if(isset($_POST['submit'])) {
             $user = escape($_POST['username']);
-			$pass = md5($_POST['password']);
+            $pass = md5($_POST['password']);
+            
+            if($user != "" && $pass != "") {
+				
+				$verify = $db->query("SELECT * FROM admin WHERE username='$user' AND password='$pass' LIMIT 1");
+				
+				if($verify->num_rows) {
+					
+					$row = $verify->fetch_assoc();
+					
+					$_SESSION['user'] = $row['username'];
+                    header("location: food_list.php");
+					
+					//echo "LOGGED IN";
+					
+				}
+				
+			}
         }
     }
 
