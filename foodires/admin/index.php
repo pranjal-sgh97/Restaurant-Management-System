adc9025b2cf71117ccd5cc548810190bd6cc5f1c
Vaibhav <vaibhav.singh_cs18@gla.ac.in>
2020-11-24 15:36:12 +0530

    isset func to start on click only

--- foodires/admin/index.php
+++ foodires/admin/index.php
@@ -3,4 +3,11 @@
 	session_start();
 	require "includes/db.php";
     require "includes/functions.php";
+    if(isset($_SESSION['user'])) {
+        header("location: food_list.php");
+    }
+    if($_SERVER['REQUEST_METHOD'] == 'POST') {
+        
+    }
+
     ?>
